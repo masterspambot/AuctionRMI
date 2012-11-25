1279c91f7ba2968091a31a0964cff26e6322064c
meshuga <patrykorwat@gmail.com>
2012-11-25 22:34:27 +0100

    scripts improved

--- src/run_server.sh
+++ src/run_server.sh
@@ -4,9 +4,9 @@ killall rmiregistry 2>/dev/null
 sleep 1
 rmiregistry >/dev/null &
 
-javac Server/AuctionServerImpl.java
-rmic Server.AuctionServerImpl
-
-javac Server/AuctionServer.java
-java Server.AuctionServer&
+javac Server/AuctionServerImpl.java &&\
+rmic Server.AuctionServerImpl &&\
+\
+javac Server/AuctionServer.java &&\
+java Server.AuctionServer &&\
 echo "Compiled and started."
