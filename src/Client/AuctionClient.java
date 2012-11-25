c70c0fbf329f79ec32d52376eaa2e02d7be4abef
meshuga <patrykorwat@gmail.com>
2012-11-25 17:10:12 +0100

    clieeent side

--- src/Client/AuctionClient.java
+++ src/Client/AuctionClient.java
@@ -1,9 +1,12 @@
 package Client;
 
-import java.rmi.*;
-import java.net.*;
 import Server.IAuctionServer;
 import Server.Item;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.rmi.*;
 import java.util.ArrayList;
 
 public class AuctionClient {
@@ -15,7 +18,16 @@ public class AuctionClient {
     ser = (IAuctionServer) ro;
   }
   
+  public void placeItemForBid(String ownerName, String itemName, String itemDesc, double startBid, int auctionTime) throws RemoteException{
+    ser.placeItemForBid(ownerName, itemName, itemDesc, startBid, auctionTime);
+  }
+  
+  public void bidOnItem(String bidderName, String itemName, double bid) throws RemoteException{
+      ser.bidOnItem(bidderName, itemName, bid);
+  }
+  
   public void getItems() throws RemoteException{
+    
     ArrayList<Item> items = ser.getItems();
     
     for(Item item : items){
@@ -27,6 +39,15 @@ public class AuctionClient {
       System.out.println("-----------------------------------------------");
     }
   }
+  
+  public void printHelp(){
+      System.out.println("Auction RMI v1");
+      System.out.println("Type a command to proceed:");
+      System.out.println("1 - Add a new auction");
+      System.out.println("2 - Bid on an item");
+      System.out.println("3 - Print items");
+      System.out.println("q - Exit");
+  }
 
   public static void main(String args[]) {
     if (args.length == 0 || !args[0].startsWith("rmi:")) {
@@ -34,9 +55,48 @@ public class AuctionClient {
       return;   
     }         
 
-    try {      
+    try {   
+        // The main app code
         AuctionClient client = new AuctionClient(args[0]);
-        client.getItems();
+        
+        client.printHelp();
+        
+        String CurLine, ownerName, itemName, itemDesc;
+        double bid;
+        int auctionTime;
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        while (true){
+            CurLine = in.readLine();
+            switch (CurLine) {
+                case "1":
+                    System.out.println("Put Owner name");
+                    ownerName = in.readLine();
+                    System.out.println("Put Item name");
+                    itemName = in.readLine(); 
+                    System.out.println("Put Item description");
+                    itemDesc = in.readLine();
+                    System.out.println("Put Starting bid");
+                    bid = Double.parseDouble(in.readLine());
+                    System.out.println("Put Time of auction (sec)");
+                    auctionTime = Integer.parseInt(in.readLine()); 
+                    client.placeItemForBid(ownerName, itemName, itemDesc, bid, auctionTime);
+                    break;
+                case "2":
+                    System.out.println("Put Owner name");
+                    ownerName = in.readLine();
+                    System.out.println("Put Item name");
+                    itemName = in.readLine();
+                    System.out.println("Put bid");
+                    bid = Double.parseDouble(in.readLine());
+                    client.bidOnItem(ownerName, itemName, bid);
+                    break;
+                case "3":
+                    client.getItems();
+                    break;
+                
+            }
+        }
+        
     }
     catch (MalformedURLException ex) {
       System.err.println(args[0] + " is not a valid RMI URL");
@@ -46,6 +106,9 @@ public class AuctionClient {
     }
     catch (NotBoundException ex) {
       System.err.println("Could not find the requested remote object on the server");
+    } 
+    catch (IOException ex) {
+      System.err.println("Error while reading input");
     } 
   }
 
